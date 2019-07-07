6f701f9ccdc6cf2dfb2177a945c181da1ead75e0
Kenneth Aalberg <kenneaal@gmail.com>
2019-07-07 23:26:46 +0200

    Fix class name lowercase. Add Platform enum.

--- apiv3mockup/models/rat.py
+++ apiv3mockup/models/rat.py
@@ -4,17 +4,26 @@ from sqlalchemy import (
     String,
     GUID,
     JSON,
+    Enum,
     DateTime)
+import enum
 
 from .meta import Base
 
 
-class rat(Base):
+class Platform(enum.Enum):
+    PS = 1,
+    PC = 2,
+    XB = 3,
+
+
+class Rat(Base):
     __tablename__ = 'rats'
     id = Column(GUID, primary_key=True)
     name = Column(String(255))
     data = Column(JSON)
     joined = Column(DateTime)
+    platform = Column(Enum(Platform))
     # TODO Absolver: platform enum! platform = Column()
     userId = Column(GUID)
 
