31b0787daad396bef96db44a5b41165119caafcd
Kenneth Aalberg <kenneaal@gmail.com>
2019-07-09 11:54:19 +0200

    Add Groups model

---
+++ apiv3mockup/models/group.py
@@ -0,0 +1,15 @@
+from sqlalchemy import (
+    Column,
+    String,
+    GUID,
+    Boolean, Integer, ARRAY)
+from .meta import Base
+
+
+class Group(Base):
+    __tablename__ = 'groups'
+    id = Column(GUID)
+    vhost = Column(String)
+    isAdministrator = Column(Boolean)
+    priority = Column(Integer)
+    permissions = Column(ARRAY)
