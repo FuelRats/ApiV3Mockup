390a2e31614efd1615ec6df1f29e9b4edc6524e1
Kenneth Aalberg <kenneaal@gmail.com>
2019-07-09 15:49:49 +0200

    ...rrrrrgh!

--- apiv3mockup/models/rescue.py
+++ apiv3mockup/models/rescue.py
@@ -1,15 +1,10 @@
 from sqlalchemy import (
     Column,
     String,
-    JSON,
     Boolean,
     Text,
-    Enum,
     ARRAY, ForeignKey)
 
-from .enums.outcome import Outcome
-from .enums.platform import Platform
-from .enums.status import Status
 from .meta import Base
 
 from sqlalchemy.dialects.postgresql import JSONB
@@ -22,7 +17,7 @@ class Rescue(Base):
     codeRed = Column(Boolean)
     data = Column(JSONB)
     notes = Column(Text)
-    platform = Column(Enum(Platform))
+    platform = Column(String)
     quotes = Column(ARRAY(JSONB))
     status = Column(String)
     system = Column(String)
